b4a5766a4712724f400f168f278a6b6db1a5d408
missile247 <amidha96@gmail.com>
2023-03-08 14:40:12 +0530

    architecture of homepage completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Admin from "./components/Admin/Admin";
+import Auth from "./components/Auth/Auth";
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import Movies from "./components/Movies/Movies";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <section>
+        <Routes>
+        <Route path="/" element={<HomePage/>} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/auth" element={<Auth/>} />
+        </Routes>
+      </section>
     </div>
   );
 }
